68403abee74afbafb3c627bb1a1539e380a4ce8e
Mingtong Zhang <71915735+mtzhang1999@users.noreply.github.com>
2022-04-27 21:01:55 +0800

    Update linear_classifier.py

--- assignments/assignment1/cs231n/classifiers/linear_classifier.py
+++ assignments/assignment1/cs231n/classifiers/linear_classifier.py
@@ -65,8 +65,10 @@ class LinearClassifier(object):
             # replacement is faster than sampling without replacement.              #
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-            pass
+            mask = np.random.choice(num_train, size=batch_size, replace=True)
+            X_batch = X[mask]
+            y_batch = y[mask]
+            # pass
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -80,8 +82,9 @@ class LinearClassifier(object):
             # Update the weights using the gradient and the learning rate.          #
             #########################################################################
             # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+            self.W += -learning_rate * grad
 
-            pass
+            # pass
 
             # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -110,8 +113,9 @@ class LinearClassifier(object):
         # Implement this method. Store the predicted labels in y_pred.            #
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
-        pass
+        scores = X.dot(self.W) # N * C
+        y_pred = np.argmax(scores, axis=1)
+        # pass
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
